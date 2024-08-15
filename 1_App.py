080d2034fa5b5130f61a2bf0c639b3d9b093514c
claudio <34164395+claudio1975@users.noreply.github.com>
2024-08-15 22:39:03 +0200

    Add files via upload

---
+++ 1_App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("# Welcome to the CO2 Emissions Prediction Web App! 👋")
+
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
+        )
+
+
