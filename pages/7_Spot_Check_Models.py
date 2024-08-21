c89d400d28eb3efaaa3646bfd5adb54cddfaf912
claudio <34164395+claudio1975@users.noreply.github.com>
2024-08-21 18:18:20 +0200

    Update 7_Spot_Check_Models.py

--- pages/7_Spot_Check_Models.py
+++ pages/7_Spot_Check_Models.py
@@ -206,14 +206,14 @@ LGBM_cqr = pickle.load(open('./data/LGBM_cqr_model.sav', 'rb'))
 LGBM_naive = pickle.load(open('./data/LGBM_naive_model.sav', 'rb'))
 LGBM_jacknife = pickle.load(open('./data/LGBM_jacknife_model.sav', 'rb'))
 LGBM_jacknife_plus = pickle.load(open('./data/LGBM_jacknife_plus_model.sav', 'rb'))
-LGBM_explainer = pickle.load(open('./data/LGBM_explainer_model.sav', 'rb'))
+#LGBM_explainer = pickle.load(open('./data/LGBM_explainer_model.sav', 'rb'))
 
 
 LR_cqr = pickle.load(open('./data/LR_cqr_model.sav', 'rb'))
 LR_naive = pickle.load(open('./data/LR_naive_model.sav', 'rb'))
 LR_jacknife = pickle.load(open('./data/LR_jacknife_model.sav', 'rb'))
 LR_jacknife_plus = pickle.load(open('./data/LR_jacknife_plus_model.sav', 'rb'))
-QR_explainer = pickle.load(open('./data/LR_explainer_model.sav', 'rb'))
+#QR_explainer = pickle.load(open('./data/LR_explainer_model.sav', 'rb'))
 
                         
 # Prediction
