b190c7eb3ec75f9a732e4d279ff22002530f3316
claudio <34164395+claudio1975@users.noreply.github.com>
2024-11-20 10:16:14 +0100

    Update 1_App.py

--- 1_App.py
+++ 1_App.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.write("# Welcome to the CO2 Emissions Prediction Web App! 👋")
+st.write("# Welcome to the CO2 Emissions by vehicles with Conformal Prediction on Regression Task! 👋")
 
 st.markdown("""
         **👈 Select a page from the dropdown on the left** to see some applications of the app!"""
         )
 
+st.markdown("""read the [article]!(https://medium.com/@c.giancaterino/predict-co2-emissions-by-vehicles-with-conformal-prediction-3d40a5e8c136)""")
 
